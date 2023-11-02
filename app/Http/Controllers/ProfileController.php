a304a662f9c5bea4eac6395a9e30965f0daff5a7
unknown <neet693@gmail.com>
2023-11-02 09:04:02 +0700

    minor bug fix bagian update password

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -20,8 +20,14 @@ class ProfileController extends Controller
         $request->validate([
             'name' => 'sometimes|string|max:255',
             'email' => 'sometimes|string|email|max:255|unique:users,email,' . $user->id,
-            'password' => 'sometimes|min:8|confirmed',
         ]);
+
+        if ($request->filled('password')) {
+            $request->validate([
+                'password' => 'required|min:8|confirmed',
+            ]);
+            $user->password = bcrypt($request->input('password'));
+        }
 
         if ($request->filled('name')) {
             $user->name = $request->input('name');
@@ -29,10 +35,6 @@ class ProfileController extends Controller
 
         if ($request->filled('email')) {
             $user->email = $request->input('email');
-        }
-
-        if ($request->filled('password')) {
-            $user->password = bcrypt($request->input('password'));
         }
 
         $user->save();
