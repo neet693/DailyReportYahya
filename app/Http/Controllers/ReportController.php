0ab4788137c533ca7b1045607a66eb101212c0a8
unknown <neet693@gmail.com>
2023-11-02 09:32:40 +0700

    Fixing ReportController

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Assignments;
+use App\Models\Assignment;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use PDF;
@@ -16,7 +16,7 @@ class ReportController extends Controller
 
         // Ambil data yang memiliki tanggal dalam rentang minggu ini
         $data = Task::whereBetween('task_date', [$startOfWeek, $endOfWeek])->get();
-        $assignments = Assignments::all();
+        $assignments = Assignment::all();
 
         $pdf = PDF::loadView('report.weekly', compact('data', 'assignments'))->setPaper('a4', 'landscape');
 
@@ -26,7 +26,7 @@ class ReportController extends Controller
     public function generateMonthlyReport()
     {
         $data = Task::whereMonth('task_date', now()->month)->get();
-        $assignments = Assignments::all();
+        $assignments = Assignment::all();
 
         $pdf = PDF::loadView('report.monthly', compact('data', 'assignments'))->setPaper('a4', 'landscape');
 
